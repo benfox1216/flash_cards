bda9e26d2381d99cf06368057e2f0fff301914db
Ben Fox <benfox1216@gmail.com>
2019-11-21 21:32:16 -0700

    Add changes to deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,18 @@
 class Deck
+  attr_reader :cards
 
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def cards_in_category(card_category)
+    cards_specified = []
+    cards.each do |card|
+      if card_category == card.category
+        cards_specified << card
+      end
+    end
+
+    p cards_specified
+  end
 end
