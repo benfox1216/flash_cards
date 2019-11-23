bee6583c5b86329b4b98e3a6dfb4114b814e6a6e
Ben Fox <benfox1216@gmail.com>
2019-11-22 23:44:19 -0700

    Add changes w/ take_turn method working

--- lib/round.rb
+++ lib/round.rb
@@ -1,26 +1,27 @@
 class Round
-  attr_accessor :deck, :turns, :number_correct
+  attr_accessor :deck, :turns, :number_correct, :current_card
   def initialize(deck)
     @deck = deck
     @turns = []
     @number_correct = 0
+    @current_card = deck.cards[0]
   end
 
   def current_card
-    deck.cards[0]
+    current_card = deck.cards[0]
   end
 
   def take_turn(guess)
-    current_card = deck.cards[0]
+    current_turn = Turn.new(guess, current_card)
+    @turns << current_turn
 
     if guess == deck.cards[0].answer
       @number_correct += 1
     end
 
-    current_turn = Turn.new(guess, current_card)
-    @turns << current_card
-
     deck.cards.shift
+
+    return current_turn
   end
 
   def number_correct_by_category
