0b7200492a11f773099291ea193ff8ef08477401
Ben Fox <benfox1216@gmail.com>
2019-11-25 01:39:03 -0700

    Final commit for submission

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -8,15 +8,15 @@ class CardGenerator
   end
 
   def cards
-    cards_from_file = []
+    @cards_from_file = []
 
     File.open("./lib/#{@text_file}").each do |line|
       card = line.split(",")
 
       new_card = Card.new(card[0], card[1], card[2].chop)
-      cards_from_file << new_card
+      @cards_from_file << new_card
     end
 
-    return cards_from_file
+    return @cards_from_file
   end
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -17,13 +17,12 @@ class CardGeneratorTest < Minitest::Test
     assert_equal "cards.txt", @card_generator.text_file
   end
 
+# TEST IF FAILING WITH THE FOLLOWING ERROR:
+# No visible difference in the Array#inspect output.
+# You should look at the implementation of #== on Array or its members.
+#
+# Apparently it means the expected value is correct, but it's still failing...unfortunately it's too late to research further before submission
   def test_it_has_cards
-
-    @card1 = @card_generator.cards[0]
-    @card2 = @card_generator.cards[1]
-    @card3 = @card_generator.cards[2]
-    @card3 = @card_generator.cards[3]
-
-    assert_equal [@card1, @card2, @card3, @card4], @card_generator.cards
+    assert_equal [@card_generator.cards[0], @card_generator.cards[1], @card_generator.cards[2], @card_generator.cards[3]], @card_generator.cards
   end
 end
